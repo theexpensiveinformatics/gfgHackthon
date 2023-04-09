006d86c0ee128fd2ed8b269de01fc1ef415a9f01
The Expensive Informatics <107309589+theexpensiveinformatics@users.noreply.github.com>
2023-04-09 22:45:38 +0530

    broadcast done

--- app/src/main/java/error404/gfg/healthcare/Authantication.java
+++ app/src/main/java/error404/gfg/healthcare/Authantication.java
@@ -129,6 +129,7 @@ public class Authantication extends AppCompatActivity {
 
                             }
 
+                            //nothing
 
                         }
                     });
