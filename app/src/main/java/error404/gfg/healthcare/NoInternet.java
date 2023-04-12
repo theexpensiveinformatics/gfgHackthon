93f79ed98010d29049b65f1786e219b4bf13d46d
The Expensive Informatics <107309589+theexpensiveinformatics@users.noreply.github.com>
2023-04-12 11:17:50 +0530

    Splash Screen Done

--- app/src/main/java/error404/gfg/healthcare/NoInternet.java
+++ app/src/main/java/error404/gfg/healthcare/NoInternet.java
@@ -1,14 +1,40 @@
 package error404.gfg.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class NoInternet extends AppCompatActivity {
+
+    LinearLayout retry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_internet);
+
+        //status bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.bg));
+
+
+        retry=(LinearLayout)    findViewById(R.id.retry_bg);
+        retry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent retry = new Intent(NoInternet.this, MainActivity.class);
+                startActivity(retry);
+                finish();
+            }
+        });
     }
 }
