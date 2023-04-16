56f8e9e28b312f6db0b01d37507b01fcc5abd5e4
The Expensive Informatics <107309589+theexpensiveinformatics@users.noreply.github.com>
2023-04-16 11:48:57 +0530

    Quiz ui

--- app/src/main/java/error404/gfg/healthcare/quizgame_box.java
+++ app/src/main/java/error404/gfg/healthcare/quizgame_box.java
@@ -1,11 +1,18 @@
 package error404.gfg.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -37,6 +44,26 @@ public class quizgame_box extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding= ActivityQuizgameBoxBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //status bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.bg));
+
+        //status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+
+        ConstraintLayout constraintLayout = findViewById(R.id.bg_quizBox);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(700);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
 
         binding.quizBtn.setOnClickListener(new View.OnClickListener() {
             @Override
